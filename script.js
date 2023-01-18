9736ff3786b16ee54005f371a8847090e3962e98
safari-zahr <zahrasafary38@gmail.com>
2023-01-18 23:27:26 +0330

    add search engine

---
+++ script.js
@@ -0,0 +1,102 @@
+ //const { default: axios } = require("axios");
+
+
+let apiKey="8f2374b487bf3796819ba34bbaf253b4";
+
+
+let now = new Date();
+
+let day = now.getDay();
+let hour = now.getHours();
+let min = now.getMinutes();
+let daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let time = "".concat(daysWeek[day], " , ").concat(hour, ":").concat(min);
+let date = document.querySelector("#date");
+date.innerHTML = time;
+let searchForm = document.querySelector("#search-form");
+let current = "isfahan";
+function showCityName(isCurrent = false) {
+console.log(isCurrent);
+  let cityInput = isCurrent ? current : document.querySelector("#city-input").value;
+  let cityName = document.querySelector("#city");
+  let weatherType = document.querySelector("#weather-type");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+ // let aa ="https://api.openweathermap.org/data/2.5/weather?q=London&appid=94e4cb4000e002bf1b316d0d3bb17f2c"
+   let URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`
+
+  axios.get(URL)
+  .then(function (response) {
+    // handle success
+
+    cityName.innerHTML= response.data.name;
+      weatherType.innerHTML = response.data.weather[0].description;
+      humidity.innerHTML = `${response.data.main.humidity}%`;
+      wind.innerHTML = `${response.data.wind.speed} Km/h`
+
+     // document.querySelector("#app").innerHTML = templateEng({city:response.data.name},document.querySelector("#app").innerHTML);
+  })
+
+  //cityName.innerHTML = cityInput; // document.getElementById("#city-input").value;
+}
+
+// function templateEng(params,html){
+//   for(param in params){
+//     var replace = "/\{\{"+params[param]+"\}\}/g";
+//     var re = new RegExp(replace,"g");
+//     html.replace(re,param);
+//   }
+//   return html;
+// }
+
+document.getElementById('location-button').addEventListener("click", () => {showCityName(false)});
+document.getElementById('current-location-button').addEventListener("click", () => {showCityName(true)});
+
+
+var apiUrlGeo = `http://api.openweathermap.org/geo/1.0/direct?q=Isfahan&appid=${apiKey}`;
+console.log(apiUrlGeo)
+
+
+function handlePosition(){
+  let cityName = document.querySelector("#city");
+ // navigator.geolocation.getCurrentPosition(showPosition)
+
+ axios.get(apiUrlGeo).then(function (response){
+  console.log(response);
+
+  cityName.innerHTML=response.data[0].name;
+  
+})
+}
+
+
+ let currentLocation= document.querySelector("#current-location-button");
+ currentLocation.addEventListener("click", handlePosition)
+
+
+// function showPosition(position) {
+//   console.log('showPosition')
+//   console.log(position)
+// }
+
+
+function tofahrenheit(event) {
+  event.preventDefault();
+  var Celsius = 17; // in Celsius
+
+  var cToFahr = Celsius * 9 / 5 + 32;
+  var temperature = document.querySelector("#temperature");
+  temperature.innerHTML = cToFahr;
+}
+
+var fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", tofahrenheit);
+var celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", tocelsius);
+
+function tocelsius(event) {
+  event.preventDefault();
+  var Celsius = 5 / 9 * (62.6 - 32);
+  var temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Celsius;
+}
