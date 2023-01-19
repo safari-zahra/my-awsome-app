0673783dd4a09a1141e37f5f00791b34e0d2f368
safari-zahr <zahrasafary38@gmail.com>
2023-01-19 23:12:02 +0330

    add icons

--- script.js
+++ script.js
@@ -13,26 +13,32 @@ let daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
 let time = "".concat(daysWeek[day], " , ").concat(hour, ":").concat(min);
 let date = document.querySelector("#date");
 date.innerHTML = time;
+
 let searchForm = document.querySelector("#search-form");
+
 let current = "isfahan";
+
 function showCityName(isCurrent = false) {
-console.log(isCurrent);
+
   let cityInput = isCurrent ? current : document.querySelector("#city-input").value;
   let cityName = document.querySelector("#city");
   let weatherType = document.querySelector("#weather-type");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+  let icon = document.querySelector("#icon");
  // let aa ="https://api.openweathermap.org/data/2.5/weather?q=London&appid=94e4cb4000e002bf1b316d0d3bb17f2c"
    let URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`
 
   axios.get(URL)
   .then(function (response) {
     // handle success
-
+      console.log(response.data.weather[0].icon);
     cityName.innerHTML= response.data.name;
       weatherType.innerHTML = response.data.weather[0].description;
       humidity.innerHTML = `${response.data.main.humidity}%`;
-      wind.innerHTML = `${response.data.wind.speed} Km/h`
+      wind.innerHTML = `${response.data.wind.speed} Km/h`;
+      icon.setAttribute("src" , `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+      icon.setAttribute("alt" , response.data.name );
 
      // document.querySelector("#app").innerHTML = templateEng({city:response.data.name},document.querySelector("#app").innerHTML);
   })
@@ -71,7 +77,7 @@ function handlePosition(){
 
 
  let currentLocation= document.querySelector("#current-location-button");
- currentLocation.addEventListener("click", handlePosition)
+ currentLocation.addEventListener("click", handlePosition);
 
 
 // function showPosition(position) {
@@ -82,21 +88,21 @@ function handlePosition(){
 
 function tofahrenheit(event) {
   event.preventDefault();
-  var Celsius = 17; // in Celsius
+  let Celsius = 17; // in Celsius
 
-  var cToFahr = Celsius * 9 / 5 + 32;
-  var temperature = document.querySelector("#temperature");
+    let cToFahr = Celsius * 9 / 5 + 32;
+    let temperature = document.querySelector("#temperature");
   temperature.innerHTML = cToFahr;
 }
 
-var fahrenheitLink = document.querySelector("#fahrenheit-link");
+ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", tofahrenheit);
 var celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", tocelsius);
 
 function tocelsius(event) {
   event.preventDefault();
-  var Celsius = 5 / 9 * (62.6 - 32);
-  var temperature = document.querySelector("#temperature");
+    let Celsius = 5 / 9 * (62.6 - 32);
+    let temperature = document.querySelector("#temperature");
   temperature.innerHTML = Celsius;
 }
